3ed8c607e1611fe11773353d275b0a85439a38cb
dohaiha1703 <dohaiha1703@gmail.com>
2018-12-18 15:51:13 +0700

    task_4388

--- app/src/main/java/com/framgia/gotosalon/data/source/AuthenicationDataSource.java
+++ app/src/main/java/com/framgia/gotosalon/data/source/AuthenicationDataSource.java
@@ -1,5 +1,6 @@
 package com.framgia.gotosalon.data.source;
 
+import com.framgia.gotosalon.data.model.Account;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 
@@ -8,5 +9,11 @@ public interface AuthenicationDataSource {
     interface Remote {
         void signInAccount(String email, String password, OnCompleteListener completeListener,
                            OnFailureListener failureListener);
+    }
+
+    interface Local {
+        void saveAccount(Account account);
+
+        void restoreAccount(Account account);
     }
 }

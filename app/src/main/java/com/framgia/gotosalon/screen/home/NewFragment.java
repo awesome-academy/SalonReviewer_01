e308e752a49eb039edf272834ef7122c5a8609e1
dohaiha1703 <dohaiha1703@gmail.com>
2019-01-02 14:54:25 +0700

    task_4397

--- app/src/main/java/com/framgia/gotosalon/screen/home/NewFragment.java
+++ app/src/main/java/com/framgia/gotosalon/screen/home/NewFragment.java
@@ -3,7 +3,6 @@ package com.framgia.gotosalon.screen.home;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
